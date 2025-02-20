a757d88c2522b4564ece53bd45d4098b445570b2
Fernando Batista <fernjbatista@gmail.com>
2025-02-20 02:22:06 +0100

    Update

--- index.js
+++ index.js
@@ -9,13 +9,7 @@ addEventListener("DOMContentLoaded", function () {
 
         // Validate Form 
         const emailInput = document.getElementById('email');
-        const emailError = document.getElementById('#mail + span.error');
-        /* emailInput.reportValidity();
-
-        //Validate with the built-it contraints
-        if (!emailInput.validity.valid) {
-            return;
-        } */
+        const emailError = document.querySelector('#email + span.error');
 
         emailInput.addEventListener('input', (event) => {
             if (emailError.validity.valid) {
@@ -27,8 +21,52 @@ addEventListener("DOMContentLoaded", function () {
                 showError();
             }
         });
+
+        form.addEventListener("submit", (event) => {
+            // if the email field is invalid
+            if (!emailInput.validity.valid) {
+                // display an appropiate error message
+                showError();
+                event.preventDefault() ;
+            } else {
+                // If the email is valid, show the success message
+                showSuccess();
+            }
+        });
+
+        addEventListener('click', function (event) {
+            if (event.target.id === 'overlay' || event.target.id === 'dismissButton') {
+                // If the user clicks outside the overlay, close it
+                const overlay = document.getElementById('overlay');
+                overlay.classList.remove('active');
+                // Reset the form
+                emailInput.value = '';
+                emailInput.classList.remove('touched');
+                emailError.textContent = '';
+            }
+        })
+
+        function showError() {
+            if (emailInput.validity.valueMissing) {
+                // if empty
+                emailError.textContent = "You need to enter an email address.";
+            } else if (emailInput.validity.typeMismatch) {
+                // if it's not an email
+                emailError.textContent = "Entered value needs to be an email address.";
+            } else if (emailInput.validity.tooShort) {
+                // if the value is too short
+                emailError.textContent = `Email should be at least 4 characters long. You entered ${emailInput.value.length} characters.`;
+            }
+            // Add the 'active' class to the error message
+            emailError.className = "error active";
+        }
+
+        function showSuccess() {
+            // Show the success message
+            const overlay = document.getElementById('overlay');
+            overlay.classList.toggle('active');
+        }
     }
 
-    
     form.addEventListener('submit', handleSubmit)
 });
